a9370e3630338cd1b60c4c1d8f621747c6174f21
Garmire group <lgarmire@gmail.com>
2020-10-19 16:50:26 -0400

    Update machine_learning11.R

--- machine_learning11.R
+++ machine_learning11.R
@@ -9,14 +9,15 @@
 lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat, 
                                         measurementLabels = lilikoilabels, 
                                         significantPathways = significantPathways, 
-                                        selectmod = 'LDA', skippam = FALSE, cvnum = 10, 
-                                        randomseed = 7, dividep = 0.8, dividseed = NULL){
+                                        selectmod = 'LDA', skippam = FALSE, cvnum = 10, times = 10,
+                                        randomseed = 2000, dividep = 0.8, dividseed = 1998){
   
   library(caret)
   library(pROC)
   library(ggplot2)
   library(gbm)
   library(PRROC)
+
   
   cancer_df <- data.frame(t((PDSmatrix[significantPathways, 
                                        ])), Label = measurementLabels, check.names = T)
@@ -33,10 +34,15 @@ lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat,
   }
   
   trainIndex <- createDataPartition(cancer_df$subtype, p = dividep, 
-                                    list = FALSE, times = 1)
-  trainDf <- cancer_df[trainIndex, ]
-  testDf <- cancer_df[-trainIndex, ]
-  
+                                    list = FALSE, times = times)
+  performance_data_final<-NULL
+  performance_data_train_final<-NULL
+  performance_data_test_final<-NULL
+  for (i in 1:times){
+  trainIndexs<-trainIndex[,i]
+  trainDf <- cancer_df[trainIndexs, ]
+  testDf <- cancer_df[-trainIndexs, ]
+
   library(MLmetrics)
   
   MySummary  <- function(data, lev = NULL, model = NULL){
@@ -81,9 +87,9 @@ lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat,
     
     cartClasses <- stats::predict(fitres, newdata = dat, type = "prob")
     cartClasses1 <- stats::predict(fitres, newdata = dat)
-    cartConfusion <- confusionMatrix(data = cartClasses1, dat$subtype)
-    cart.ROC <- roc(predictor = cartClasses$Normal, response = dat$subtype, 
-                    levels = rev(levels(dat$subtype)))
+    cartConfusion <- confusionMatrix(data = cartClasses1, as.factor(dat$subtype))
+    cart.ROC <- roc(predictor = cartClasses$Normal, response =  as.factor(dat$subtype), 
+                    levels = rev(levels( as.factor(dat$subtype))))
     blank[1, colidx] <- as.numeric(cart.ROC$auc)
     blank[2, colidx] <- cartConfusion$byClass[1]
     blank[3, colidx] <- cartConfusion$byClass[2]
@@ -206,26 +212,26 @@ lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat,
     NULL
   })
   
-  p6 <- graphics::plot(graphics::plot(varImp(fit.lda, scale = FALSE, 
-                                             top = 20), main = "LDA"))
-  p7 <- graphics::plot(graphics::plot(varImp(fit.svm, scale = FALSE, 
-                                             top = 20), main = "SVM"))
-  p8 <- graphics::plot(graphics::plot(varImp(fit.rf, scale = FALSE, 
-                                             top = 20), main = "RF"))
-  p9 <- graphics::plot(graphics::plot(varImp(fit.gbm, scale = FALSE, 
-                                             top = 20), main = "GBM"))
-  p10 <- graphics::plot(graphics::plot(varImp(fit.pam, scale = FALSE, 
-                                              top = 20), main = "PAM"))
-  p11 <- graphics::plot(graphics::plot(varImp(fit.log, scale = FALSE, 
-                                              top = 20), main = "LOG"))
+  # p6 <- graphics::plot(graphics::plot(varImp(fit.lda, scale = FALSE, 
+  #                                            top = 20), main = "LDA"))
+  # p7 <- graphics::plot(graphics::plot(varImp(fit.svm, scale = FALSE, 
+  #                                            top = 20), main = "SVM"))
+  # p8 <- graphics::plot(graphics::plot(varImp(fit.rf, scale = FALSE, 
+  #                                            top = 20), main = "RF"))
+  # p9 <- graphics::plot(graphics::plot(varImp(fit.gbm, scale = FALSE, 
+  #                                            top = 20), main = "GBM"))
+  # p10 <- graphics::plot(graphics::plot(varImp(fit.pam, scale = FALSE, 
+  #                                             top = 20), main = "PAM"))
+  # p11 <- graphics::plot(graphics::plot(varImp(fit.log, scale = FALSE, 
+  #                                             top = 20), main = "LOG"))
   
   
   
   getROC <- function(fitres = fit.cart, dat = testDf){
     
     resClasses <- stats::predict(fitres, newdata = dat, type = "prob")
-    res.ROC <- roc(predictor = resClasses$Normal, response = dat$subtype, 
-                    levels = rev(levels(dat$subtype)))
+    res.ROC <- roc(predictor = resClasses$Normal, response = as.factor(dat$subtype), 
+                   levels = rev(levels(as.factor(dat$subtype))))
     
     return(res.ROC)
     
@@ -243,8 +249,7 @@ lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat,
   
   log.ROC <- getROC(fitres = fit.log, dat = testDf)
   
-  
-  
+
   smooth_method <- "binormal"
   graphics::plot(cart.ROC, col = "red", cex.lab = 1.5)
   graphics::par(new = TRUE)
@@ -311,7 +316,7 @@ lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat,
     prf <- plot.scores.AUC()
     
     return(list(probj = probj, prplotobj = prf))
-
+    
     
   }
   
@@ -337,7 +342,7 @@ lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat,
   }, error = function(err){
     NULL
   })
-   
+  
   if(skippam == FALSE){
     pam.PRROC <- getPRROC(fitres = fit.pam, dat = testDf)
   }
@@ -360,8 +365,7 @@ lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat,
   
   
   
-  makeperformance <- function(mat = performance, datname = 'performance'){
-    
+  makeperformance <- function(mat = performance, datname = 'Performance'){
     performance_list <- list()
     performance_list[[1]] <- mat
     list_performance <- performance_list
@@ -371,26 +375,26 @@ lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat,
     SENS_performance <- lapply(list_performance, function(x) x[2, ])
     
     SPEC_performance <- lapply(list_performance, function(x) x[3, ])
-
+    
     F1_performance <- lapply(list_performance, function(x) x[7, ])
-
+    
     Balanced_accuracy_performance <- lapply(list_performance, function(x) x[8, ])
     
     outputauc <- do.call(rbind, lapply(AUC_performance, matrix, ncol = 7, 
                                        byrow = TRUE))
-
+    
     outputsens <- do.call(rbind, lapply(SENS_performance, matrix, ncol = 7, 
                                         byrow = TRUE))
     
     outputspec <- do.call(rbind, lapply(SPEC_performance, matrix, ncol = 7, 
                                         byrow = TRUE))
-
+    
     outputf1 <- do.call(rbind, lapply(F1_performance, matrix, ncol = 7, 
                                       byrow = TRUE))
-
+    
     outputacc <- do.call(rbind, lapply(Balanced_accuracy_performance, 
                                        matrix, ncol = 7, byrow = TRUE))
-
+    
     
     AUC_mean <- apply(outputauc, 2, mean)
     SENS_mean <- apply(outputsens, 2, mean)
@@ -412,24 +416,98 @@ lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat,
     return(performance_data)
   }
   
-  performance_data <- makeperformance(mat = performance, datname = 'performance')
-  performance_data_train <- makeperformance(mat = performance_training, datname = 'training')
-  performance_data_test <- makeperformance(mat = performance_testing, datname = 'testing')
+  performance_data <- makeperformance(mat = performance, datname = 'Performance')
+  performance_data_train <- makeperformance(mat = performance_training, datname = 'Training')
+  performance_data_test <- makeperformance(mat = performance_testing, datname = 'Testing')
   
-  meltdata <- function(performance_data = performance_data, algname = NULL){
-    
+  performance_data$Repeats<-i
+  performance_data_train$Repeats<-i
+  performance_data_test$Repeats<-i
+  
+  performance_data_final<-rbind(performance_data_final,performance_data)
+  performance_data_train_final<-rbind(performance_data_train_final,performance_data_train)
+  performance_data_test_final<-rbind(performance_data_test_final,performance_data_test)
+  }  
+  performance_data_final[is.na(performance_data_final)]<-0
+  performance_data_train_final[is.na(performance_data_train_final)]<-0
+  performance_data_test_final[is.na(performance_data_test_final)]<-0
+  performance_mean<-function(performance_data_final=performance_data){
+    AUC_mean_final<-tapply(performance_data_final$AUC,performance_data_final$Algorithm,mean)
+    SENS_mean_final<-tapply(performance_data_final$SENS,performance_data_final$Algorithm,mean)
+    SPEC_mean_final<-tapply(performance_data_final$SPEC,performance_data_final$Algorithm,mean)
+    F1_mean_final<-tapply(performance_data_final$F1,performance_data_final$Algorithm,mean)
+    Balanced_accuracy_mean_final<-tapply(performance_data_final$Balanced_accuracy,performance_data_final$Algorithm,mean)
+    performance_data_final_mean <- data.frame(AUC = AUC_mean_final, 
+                                   F1 = F1_mean_final, 
+                                   Balanced_accuracy = Balanced_accuracy_mean_final, 
+                                   datname = unique(performance_data_final$datname), 
+                                   Algorithm = names(AUC_mean_final))
+    return(performance_data_final_mean)
+  }
+  performance_selection<-function(performance_data_final=performance_data){
+    performance_data_final<-performance_data_final[which(rowSums(performance_data_final==0)==0),]
+    AUC_final<-tapply(performance_data_final$AUC,performance_data_final$Repeats,sum)
+    SENS_final<-tapply(performance_data_final$SENS,performance_data_final$Repeats,sum)
+    SPEC_final<-tapply(performance_data_final$SPEC,performance_data_final$Repeats,sum)
+    F1_mean_final<-tapply(performance_data_final$F1,performance_data_final$Repeats,sum)
+    Balanced_accuracy_mean_final<-tapply(performance_data_final$Balanced_accuracy,performance_data_final$Repeats,sum)
+    performance_scores <- cbind(AUC_final,F1_mean_final,Balanced_accuracy_mean_final)
+    performance_score <-apply(performance_scores, 1, sum)
+    performance_data_selected<-unique(subset(performance_data_final, Repeats==which.max(performance_score)))
+    new.list<-colnames(performance_data_selected)
+    new.list<-setdiff(new.list,c("SENS","SPEC","Repeats"))
+    performance_data_selected<-performance_data_selected[,new.list]
+    return(performance_data_selected)
+  }
+  if(mean(performance_data_test_final$F1)<0.5){
+  performance_data <- performance_mean(performance_data_final)
+  performance_data_train <- performance_mean(performance_data_train_final)
+  performance_data_test <- performance_mean(performance_data_test_final)
+  }else{
+    performance_data <- performance_selection(performance_data_final)
+    performance_data_train <- performance_selection(performance_data_train_final)
+    performance_data_test <- performance_selection(performance_data_test_final)
+  }
+  performance_sd<-function(performance_data_final=performance_data){
+    library(Rmisc)
+    AUC_sd <- summarySE(performance_data_final, measurevar = "AUC",groupvars = "Algorithm")$sd
+    SENS_sd <- summarySE(performance_data_final, measurevar = "SENS",groupvars = "Algorithm")$sd
+    SPEC_sd <- summarySE(performance_data_final, measurevar = "SPEC",groupvars = "Algorithm")$sd
+    F1_sd <- summarySE(performance_data_final, measurevar = "F1",groupvars = "Algorithm")$sd
+    Balanced_accuracy_sd <- summarySE(performance_data_final, measurevar = "Balanced_accuracy",groupvars = "Algorithm")$sd
+    performance_data_final_sd <- data.frame(AUC = AUC_sd, 
+                                              F1 = F1_sd, 
+                                              Balanced_accuracy = Balanced_accuracy_sd, 
+                                              datname = unique(performance_data_final$datname), 
+                                              Algorithm = summarySE(performance_data_final, measurevar = "AUC",groupvars = "Algorithm")$Algorithm)
+    performance_data_final_sd[is.na(performance_data_final_sd)]<-0
+    return(performance_data_final_sd)
+  }
+  performance_data_sd <- performance_sd(performance_data_final)
+  performance_data_train_sd <- performance_sd(performance_data_train_final)
+  performance_data_test_sd <- performance_sd(performance_data_test_final) 
+  
+  meltdata <- function(performance_data = performance_data, performance_data_sd = performance_data_sd, algname = NULL){
+    rownames(performance_data_sd) <-performance_data_sd$Algorithm
+    performance_data_sd<-performance_data_sd[performance_data$Algorithm,]
     library(reshape)
     
     performance_data <- performance_data[order(-performance_data$F1),]
     modorder <- as.character(performance_data$Algorithm)
     melted_performance_data <- suppressMessages(melt(performance_data))
-
+    melted_performance_data_sd <- suppressMessages(melt(performance_data_sd))
+    temp<-1-melted_performance_data$value
+    temp.table<-data.frame(temp1=temp,temp2=melted_performance_data_sd$value)
+    final.sd<-apply(temp.table,1,min)
+    melted_performance_data$SD <- final.sd
+    
+    
     Algorithm <- NULL
     value <- NULL
     variable <- NULL
     textLabels <- geom_text(aes(x = Algorithm, label = round(value, 2)), 
-                            position = position_dodge(width = 1), 
-                            vjust = -0.5, size = 2)
+                            position = position_dodge(width = 0.9), 
+                            vjust = -0.5, size = 3)
     
     melted_performance_data$Algorithm <- factor(melted_performance_data$Algorithm, levels = modorder, 
                                                 ordered = TRUE)
@@ -440,6 +518,8 @@ lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat,
     
     p <- ggplot(data = melted_performance_data, aes(x = Algorithm, y = value, fill = variable)) + 
       geom_bar(stat = "identity", position = position_dodge()) + 
+      geom_errorbar(aes(ymin = value, ymax = value + SD), 
+                    width = 0.2, position = position_dodge(0.9),alpha = 0.3) +
       xlab("") + ylab("") + ggtitle(algname) + 
       theme(plot.title = element_text(hjust = 0.5), 
             axis.text = element_text(size = 15, face = "bold"), 
@@ -451,24 +531,14 @@ lilikoi.machine_learning11 <- function (PDSmatrix = lilikoimat,
     
   }
   
-  p_performance <- meltdata(performance_data = performance_data)
-  
-  p_training <- meltdata(performance_data = performance_data_train)
-  
-  p_testing <- meltdata(performance_data = performance_data_test)
+  p_performance <- meltdata(performance_data = performance_data, performance_data_sd = performance_data_sd)
+  p_training <- meltdata(performance_data = performance_data_train, performance_data_sd = performance_data_train_sd)
+  p_testing <- meltdata(performance_data = performance_data_test, performance_data_sd = performance_data_test_sd)
   
   
-  best_model <- models[which.max(performance_testing[1, ])]
-  method <- (unlist(best_model)[[1]])
-  if (method == "glmnet") {
-    method <- "log"
-  }
-  if (method == "svmRadial") {
-    method <- "svm"
-  }
-  
-  dd <- subset(performance_data_test, Algorithm == toupper(method))
-  p_best <- meltdata(performance_data = dd)
+  dd <- subset(performance_data_test, Algorithm == selectmod)
+  dd_sd <- subset(performance_data_test_sd, Algorithm == selectmod)
+  p_best <- meltdata(performance_data = dd, performance_data_sd = dd_sd)
   
   
   mlResults <- list()
